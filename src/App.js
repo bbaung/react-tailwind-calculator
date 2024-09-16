ecff4f221c0c409f4ed92e1dffa6027137135937
bbsuzan <zans6791@gmail.com>
2024-09-16 16:41:46 +0630

    second pc test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="py-5">
-      <div className="w-[1280px] mx-auto bg-gray-50">
+    <div className="py-5 bg-slate-500">
+      <div className="w-[1280px] mx-auto">
 
         <div className="flex flex-col w-full ml:w-80 h-[1500px] ml:h-auto mx-auto">
           <Calculator/>
